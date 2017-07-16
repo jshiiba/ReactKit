62aecd65ae5047910423c24781d8722f27ff6054
Justin Shiiba <jshiiba@gmail.com>
2017-07-15 20:41:15 -0400

    Reconciler diffs by proptypes, collectionview only updates changes

--- ReactKit/ComponentFlowLayout.swift
+++ ReactKit/ComponentFlowLayout.swift
@@ -18,12 +18,4 @@ class ComponentFlowLayout: UICollectionViewFlowLayout {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func initialLayoutAttributesForAppearingItem(at itemIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-
-        let attributes = UICollectionViewLayoutAttributes()
-        //attributes.size = CGSize(width: 300, height: 100)
-
-        return attributes
-    }
 }
--- ReactKit/Reconciler/ComponentReconciler.swift
+++ ReactKit/Reconciler/ComponentReconciler.swift
@@ -13,16 +13,35 @@ import Foundation
 /// - returns: IndexPaths to update
 class ComponentReconciler {
     func reconcile(_ currentSections: [SectionComponent], cachedSections: [SectionComponent]?) -> [IndexPath] {
-        // TODO: Diff
+        guard let cachedSections = cachedSections else {
+            return currentSections
+            // convert sections to array of rows to update
+            .reduce([]) { (result, section) in
+                return result + section.rows
+            }
+            // convert rows to IndexPath
+            .map { path in
+                return IndexPath(row: path.row, section: path.section)
+            }
+        }
 
-        return currentSections
-        // convert sections to array of rows to update
-        .reduce([]) { (result, section) in
-            return result + section.rows
+        var updatedIndexPaths: [IndexPath] = []
+
+        // TODO: improve algorithm
+        for (sectionIndex, _) in currentSections.enumerated() {
+            for (rowIndex, row) in currentSections[sectionIndex].rows.enumerated() {
+                let currentProps = row.props
+                let cachedProps = cachedSections[sectionIndex].rows[rowIndex].props
+
+                if currentProps.isEqualTo(other: cachedProps) {
+                    print("EQUAL")
+                } else {
+                    print("NOT EQUAL")
+                    updatedIndexPaths.append(IndexPath(row: row.row, section: row.section))
+                }
+            }
         }
-        // convert rows to IndexPath
-        .map { path in
-            return IndexPath(row: path.row, section: path.section)
-        }
+
+        return updatedIndexPaths
     }
 }
