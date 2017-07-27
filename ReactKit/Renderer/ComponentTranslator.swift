99c88d8904c416cef84a4f5b7b8b5391c1e4bfeb
Justin Shiiba <jshiiba@gmail.com>
2017-07-27 07:51:13 -0400

    initial refactor of translator

--- ReactKit/Renderer/ComponentTranslator.swift
+++ ReactKit/Renderer/ComponentTranslator.swift
@@ -8,52 +8,81 @@
 
 import UIKit
 
+struct Attributes {
+    let sectionIndex: Int
+    let rowIndex: Int
+    let frame: CGRect
+}
+
 ///
 ///
 ///
-class ComponentTranslator {
+final class ComponentTranslator {
     private let rootSection: Int = 0 // TODO: figure out how to avoid Section 0 always being empty
 
     func translateToSections(from rootComponent: Component, in frame: CGRect) -> [SectionComponent] {
+        guard let container = rootComponent.render() as? Container else {
+            return [] // TODO: for now only worry about containers or single components
+        }
 
-        let defaultLayout = SectionComponentLayout(dimension: .fill, parentFrame: frame)
-        var sections: [SectionComponent] = [SectionComponent(index: rootSection, rows: [], layout: defaultLayout)]
+        let attributes = Attributes(sectionIndex: 0,
+                                    rowIndex: 0,
+                                    frame: frame)
 
-        translate(rootComponent, into: &sections, at: rootSection, within: frame)
+        let sections: [SectionComponent] = translateContainer(container, with: attributes)
+        // sort by section index
+        return sections.sorted(by: { $0.index < $1.index })
+    }
 
+
+    func translateContainer(_ container: Container, with attributes: Attributes) -> [SectionComponent] {
+        var currentRows: [RowComponent] = []
+        var currentRowIndex = 0
+        var sections: [SectionComponent] = []
+        let currentAttributes = attributes
+        var currentSectionIndex = attributes.sectionIndex
+
+        // recurse on components in container for rows
+        container.components.forEach { baseComponent in
+            switch baseComponent.componentType {
+            case .component(let component):
+                let newRowAttributes = Attributes(sectionIndex: currentAttributes.sectionIndex,
+                                                  rowIndex: currentRowIndex,
+                                                  frame: attributes.frame)
+                if let single = component as? SingleViewComponent,
+                   let row = translateSingleViewComponent(single, with: newRowAttributes) {
+                    currentRows.append(row)
+                    currentRowIndex = currentRowIndex + 1
+                }
+            case .container(let container):
+                currentSectionIndex = currentSectionIndex + 1
+                let newSectionAttributes = Attributes(sectionIndex: currentSectionIndex,
+                                                      rowIndex: 0,
+                                                      frame: currentAttributes.frame)
+                let childSections = translateContainer(container, with: newSectionAttributes) // recurse
+                sections.append(contentsOf: childSections)
+            }
+        }
+
+        let layout = SectionComponentLayout(dimension: container.layout.dimension, parentFrame: currentAttributes.frame)
+        let currentSection = SectionComponent(index: currentAttributes.sectionIndex,
+                                              rows: currentRows,
+                                              layout: layout)
+        sections.append(currentSection)
         return sections
     }
 
-    func translate(_ base: BaseComponent, into sections: inout [SectionComponent], at sectionIndex: Int, within parentFrame: CGRect) {
-        switch base.componentType {
-        case .component(let component): translate(component, into: &sections, at: sectionIndex, within: parentFrame)
-        case .container(let container): translate(container, into: &sections, at: sectionIndex, within: parentFrame)
-        }
+    func translateSingleViewComponent(_ component: SingleViewComponent, with attributes: Attributes) -> RowComponent? {
+        let props = (component as! Component).props
+
+        return createRow(for: component, with: props, attributes: attributes)
     }
 
-    func translate(_ component: Component, into sections: inout [SectionComponent], at sectionIndex: Int, within parentFrame: CGRect) {
-        if let singleComponent = component as? SingleViewComponent {  // Base Case
-
-            let row = RowComponent(view: singleComponent.reduce(),
-                                   props: component.props,
-                                   layout: RowComponentLayout(dimension: component.props.layout?.dimension, sectionFrame: parentFrame),
-                                   index: sections[sectionIndex].rows.count,
-                                   section: sectionIndex)
-
-            sections[sectionIndex].rows.append(row)
-
-        } else if let child = component.render() {
-            translate(child, into: &sections, at: sectionIndex, within: parentFrame)
-        }
-    }
-
-    func translate(_ container: Container, into sections: inout [SectionComponent], at sectionIndex: Int, within parentFrame: CGRect) {
-        let newSectionIndex = sectionIndex + 1
-        let newSectionLayout = SectionComponentLayout(dimension: container.layout.dimension, parentFrame: parentFrame)
-        let newSection = SectionComponent(index: newSectionIndex, rows: [], layout: newSectionLayout)
-        sections.append(newSection)
-        container.components.forEach { component in
-            translate(component, into: &sections, at: newSectionIndex, within: newSection.layout.frame)
-        }
+    func createRow(for component: SingleViewComponent, with props: PropType, attributes: Attributes) -> RowComponent {
+        return RowComponent(view: component.reduce(),
+                            props: props,
+                            layout: RowComponentLayout(dimension: props.layout?.dimension, sectionFrame: attributes.frame),
+                            index: attributes.rowIndex,
+                            section: attributes.sectionIndex)
     }
 }
