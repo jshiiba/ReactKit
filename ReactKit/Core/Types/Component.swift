669478fd4015dc493491dfec10ad98a15c91ab24
Justin Shiiba <jshiiba@gmail.com>
2017-08-04 17:48:26 -0400

    remove componentType file

--- ReactKit/Core/Types/Component.swift
+++ ReactKit/Core/Types/Component.swift
@@ -16,6 +16,28 @@ protocol Component {
     var props: PropType { get }
 }
 
+///
+/// Allows for easier access to cast the base into its true type
+///
+enum ComponentType {
+
+    case composite(CompositeComponent)
+    case container(ComponentContaining)
+    case view(ComponentReducing)
+
+    init(component: Component) {
+        if let container = component as? ComponentContaining {
+            self = .container(container)
+        } else if let composite = component as? CompositeComponent {
+            self = .composite(composite)
+        } else if let view = component as? ComponentReducing {
+            self = .view(view)
+        } else {
+            fatalError("Component type must be of View, Composite, or Container")
+        }
+    }
+}
+
 extension Component {
     var type: ComponentType {
         return ComponentType(component: self)
